82fbfb91fc3f5938aa63f6e91b8a5b0917a1d53a
johana <johana.ceballos@reclameaqui.com.br>
2024-02-22 14:45:42 -0300

    Exercicios basicos

---
+++ src/main/java/org/example/exercicio1/basic/Exercicio23.java
@@ -0,0 +1,19 @@
+package org.example.exercicio1.basic;
+
+import java.util.Scanner;
+
+public class Exercicio23 {
+    public static void main(String[] args) {
+        Scanner scnanner = new Scanner(System.in);
+        int A , B, C;
+        A = scnanner.nextInt();
+        B = scnanner.nextInt();
+        C = scnanner.nextInt();
+
+        int maiorAB = ((A + B + Math.abs(A-B))/2);
+        int maiorResusul = ((maiorAB + C + Math.abs(maiorAB-C))/2);
+
+        System.out.println(maiorResusul + " eh o maior");
+
+    }
+}
